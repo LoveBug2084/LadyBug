2b2056f986c76cbe1d3411f3d28bef8b8e3a42ee
Galaxip <gamelovebug@gmail.com>
2022-10-25 13:30:55 +0100

    update to build.asm

--- source/build.asm
+++ source/build.asm
@@ -69,15 +69,16 @@
 	save		"EditorM", editorStart, editorEnd, &ffffff, 0
 
 	;---------------------------------------------------------------------------------------------------------------------------------------------
-	; load the bbc editor tiles screenshot saved by image2bbc.exe and save just the 19 tiles
+	; load the bbc editor tiles contained in the 20K mode 1 image saved by image2bbc.exe and save just the 19 tiles
 	;---------------------------------------------------------------------------------------------------------------------------------------------
 
-	org 0					; clear 20K of space for screenshot
+	org 0					; clear 20K of space for the bbc mode 1 image file
 	clear 0, (20 * 1024) - 1
 
-	incbin		"img-editor-tiles.bin"	; load the 20K screenshot
+	incbin		"img-editor-tiles.bin"	; load the image file
 
-						; save the 12*8 mode 1 pixel editor tiles (19 tiles total, 4 pixels per byte)
+						; save the 19 tiles of 12x8 pixels @ 4 pixels per byte (mode 1)
+
 	save		"EditorT", 0, 19 * 12 * 8 / 4, &ffffff, 0
 
 
