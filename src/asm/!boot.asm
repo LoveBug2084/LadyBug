0f4badfa82ed8255b0c1ee4ffea5f2c9c32cadc5
LoveBug <87623090+LoveBug2084@users.noreply.github.com>
2024-08-02 01:11:18 +0100

    fix
    fixed mistake in !Boot
    and updated CheckFS

--- src/asm/!boot.asm
+++ src/asm/!boot.asm
@@ -32,7 +32,7 @@
 	equb 23,1,0,0,0,0,0,0,0,0		; cursor off
 	equb 23,0,1,0,0,0,0,0,0,0		; disable display
 
-	equs "CHAIN",quote,"Menu",quote,13	; CHAIN"Menu"
+	equs "CHAIN",quote,"CheckFS",quote,13	; CHAIN"CheckFS"
 
 
 
