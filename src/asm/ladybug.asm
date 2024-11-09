868fa0e33704071c0c2f20eba392630f236fc54d
LoveBug <87623090+LoveBug2084@users.noreply.github.com>
2024-11-09 02:55:29 +0000

    changed demo text
    DEMO GAME changed to DEMO MODE

--- src/asm/ladybug.asm
+++ src/asm/ladybug.asm
@@ -3205,7 +3205,7 @@ bonusBitsMultiplier	= %00000111		; bit mask for x2x3x5 multiplier bits on bonusB
 
 	jsr drawString				; draw demo mode message
 	equw screenAddr + 2 + 16 + 5 * chrColumn + 25 * chrRow
-	equs colorMultiplier0, " DEMO GAME", &ff
+	equs colorMultiplier0, " DEMO MODE", &ff
 
 	rts
 
